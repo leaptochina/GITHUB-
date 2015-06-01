3092d222be62a71f7e5674ebd7b504db9fe6ce2f
leaptochina <pine.linhai@gmail.com>
2015-06-01 11:38:28 +0800

    提交Jun 01 11:38:28 2015

--- GithubFillTimes/GithubFillTimes/bin/Debug/runme.bat
+++ GithubFillTimes/GithubFillTimes/bin/Debug/runme.bat
@@ -1,4 +1,4 @@
 cd ../../../../
 git add .
-git commit --date="May 31 23:14:59 2015 + 0800 " -am "提交May 31 23:14:59 2015"
+git commit --date="Jun 01 11:38:28 2015 + 0800 " -am "提交Jun 01 11:38:28 2015"
 
