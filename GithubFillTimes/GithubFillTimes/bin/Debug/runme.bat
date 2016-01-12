8a64ff85ccee5c0d16b2e1f46259f1fb57f642fe
leaptochina <pine.linhai@gmail.com>
2016-01-12 18:06:02 +0800

    提交Jan 12 18:06:02 2016

--- GithubFillTimes/GithubFillTimes/bin/Debug/runme.bat
+++ GithubFillTimes/GithubFillTimes/bin/Debug/runme.bat
@@ -1,4 +1,4 @@
 cd ../../../../
 git add .
-git commit --date="Jan 12 01:08:04 2016 + 0800 " -am "提交Jan 12 01:08:04 2016"
+git commit --date="Jan 12 18:06:02 2016 + 0800 " -am "提交Jan 12 18:06:02 2016"
 
